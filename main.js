29828f7bd588ca2e1c75276153cdcbb1c005ffe8
JonahD <jonahd97@gmail.com>
2016-01-23 15:23:51 -0500

    Testing

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 document.write("You still there?")
 
-// var explain? = prompt("Do you want a brief explanation of the problem?")
+var explain? = prompt("Do you want a brief explanation of the problem?")
 
-// if (explain? === "yes" || explain? === "Yes" || explain? === "yeah" || explain? === "Yeah" || explain? === "sure" || explain? === "Sure") {
-// 	document.write("The Monty Hall problem states that if you have 3 options...");
-// } else if (explain? === "No" || explain? === "no" || explain? === "nah" || explain? === "Nah" || explain? === "Nope" || explain? === "nope") {
-// 	startDemo();
-// } else {
-// 	document.write("I don't understand what " + explain? + " means. Please try again.")
-// 	//do stuff
-// }
-
+if (explain? === "yes" || explain? === "Yes" || explain? === "yeah" || explain? === "Yeah" || explain? === "sure" || explain? === "Sure") {
+	document.write("The Monty Hall problem states that if you have 3 options...");
+} else if (explain? === "No" || explain? === "no" || explain? === "nah" || explain? === "Nah" || explain? === "Nope" || explain? === "nope") {
+	startDemo();
+} else {
+	document.write("I don't understand what " + explain? + " means. Sorry.")
+	//do stuff
+}
